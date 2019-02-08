b0cd4a085cd06b0330ee499f25e3d15b4155aaed
karabomaimane <maimane23@hotmail.com>
2019-02-08 09:55:13 +0200

    Backend database Updates

--- src/app/database.service.ts
+++ src/app/database.service.ts
@@ -12,15 +12,17 @@ export class DatabaseService {
   DjsArray = new Array();
   houseDjsArray = new Array();
   hipHopDjsArray = new Array();
-
+  RnBDjsArray = new Array();
+  DeepHouseDjsArray = new Array();
+  state;
   constructor() { }
 
-  retreiveDJs(){
-    return new Promise((accpt,rej)=>{
-      this.database.ref('Registration/').on('value', (data:any)=>{
+  retreiveDJs() {
+    return new Promise((accpt, rej) => {
+      this.database.ref('Registration/').on('value', (data: any) => {
         var Djs = data.val();
         var keys = Object.keys(Djs)
-        for(var x = 0; x < keys.length;x++){
+        for (var x = 0; x < keys.length; x++) {
           var k = keys[x];
           let obj = {
             bio: Djs[k].bio,
@@ -34,27 +36,22 @@ export class DatabaseService {
             role: Djs[k].role,
             stagename: Djs[k].stagename
           }
-          if(obj.role != null || obj.role != undefined){
-            // if(obj.genre == "Hip-Hop"){
-              //
-           // }
-            //if(obj.genre == "Commercial house"){}
+          if (obj.role != null || obj.role != undefined) {
             this.DjsArray.push(obj)
-            console.log(this.DjsArray)
           }
         }
       })
       accpt(this.DjsArray)
     })
-    
+
   }
 
-  houseDjs(){
-    return new Promise((accpt,rej)=>{
-      this.database.ref('Registration/').on('value', (data:any)=>{
+  houseDjs() {
+    return new Promise((accpt, rej) => {
+      this.database.ref('Registration/').on('value', (data: any) => {
         var Djs = data.val();
         var keys = Object.keys(Djs)
-        for(var x = 0; x < keys.length;x++){
+        for (var x = 0; x < keys.length; x++) {
           var k = keys[x];
           let obj = {
             bio: Djs[k].bio,
@@ -68,25 +65,23 @@ export class DatabaseService {
             role: Djs[k].role,
             stagename: Djs[k].stagename
           }
-          if(obj.role != null || obj.role != undefined){
-             if(obj.genre == "Commercial house"){
+          if (obj.role != null || obj.role != undefined) {
+            if (obj.genre == "Commercial house") {
               this.houseDjsArray.push(obj)
             }
-            //if(obj.genre == "Commercial house"){}
-            // this.DjsArray.push(obj)
-            console.log(this.houseDjsArray)
           }
         }
       })
       accpt(this.houseDjsArray)
     })
   }
-  HipHopDjs(){
-    return new Promise((accpt,rej)=>{
-      this.database.ref('Registration/').on('value', (data:any)=>{
+
+  DeepHouseDjs() {
+    return new Promise((accpt, rej) => {
+      this.database.ref('Registration/').on('value', (data: any) => {
         var Djs = data.val();
         var keys = Object.keys(Djs)
-        for(var x = 0; x < keys.length;x++){
+        for (var x = 0; x < keys.length; x++) {
           var k = keys[x];
           let obj = {
             bio: Djs[k].bio,
@@ -100,13 +95,40 @@ export class DatabaseService {
             role: Djs[k].role,
             stagename: Djs[k].stagename
           }
-          if(obj.role != null || obj.role != undefined){
-             if(obj.genre == "Hip-Hop"){
+          if (obj.role != null || obj.role != undefined) {
+            if (obj.genre == "Deep House") {
+              this.DeepHouseDjsArray.push(obj)
+            }
+          }
+        }
+      })
+      accpt(this.DeepHouseDjsArray)
+    })
+  }
+
+  HipHopDjs() {
+    return new Promise((accpt, rej) => {
+      this.database.ref('Registration/').on('value', (data: any) => {
+        var Djs = data.val();
+        var keys = Object.keys(Djs)
+        for (var x = 0; x < keys.length; x++) {
+          var k = keys[x];
+          let obj = {
+            bio: Djs[k].bio,
+            city: Djs[k].city,
+            email: Djs[k].email,
+            fullname: Djs[k].fullname,
+            gender: Djs[k].gender,
+            genre: Djs[k].genre,
+            payment: Djs[k].payment,
+            price: Djs[k].price,
+            role: Djs[k].role,
+            stagename: Djs[k].stagename
+          }
+          if (obj.role != null || obj.role != undefined) {
+            if (obj.genre == "Hip-Hop") {
               this.hipHopDjsArray.push(obj)
             }
-            //if(obj.genre == "Commercial house"){}
-            // this.DjsArray.push(obj)
-            console.log(this.hipHopDjsArray)
           }
         }
       })
@@ -114,9 +136,57 @@ export class DatabaseService {
     })
   }
 
-  signIn(){
-
-
+  RnBDjs() {
+    return new Promise((accpt, rej) => {
+      this.database.ref('Registration/').on('value', (data: any) => {
+        var Djs = data.val();
+        var keys = Object.keys(Djs)
+        for (var x = 0; x < keys.length; x++) {
+          var k = keys[x];
+          let obj = {
+            bio: Djs[k].bio,
+            city: Djs[k].city,
+            email: Djs[k].email,
+            fullname: Djs[k].fullname,
+            gender: Djs[k].gender,
+            genre: Djs[k].genre,
+            payment: Djs[k].payment,
+            price: Djs[k].price,
+            role: Djs[k].role,
+            stagename: Djs[k].stagename
+          }
+          if (obj.role != null || obj.role != undefined) {
+            if (obj.genre == "RnB") {
+              this.RnBDjsArray.push(obj)
+            }
+          }
+        }
+      })
+      accpt(this.RnBDjsArray)
+    })
   }
 
+  Login(email, password) {
+    return new Promise((accpt, rej) => {
+      this.authentication.signInWithEmailAndPassword(email, password).then(() => {
+        accpt("Success")
+      }, Error => {
+        alert("Something Went wrong!!!")
+      })
+    })
+  }
+
+  getUserState() {
+    return new Promise((accpt, rej) => {
+      this.authentication.onAuthStateChanged(user => {
+        if (user != null || user != undefined) {
+          this.state = 1;
+        }
+        else {
+          this.state = 0;
+        }
+      })
+      accpt(this.state)
+    })
+  }
 }
